1eae0cfe876a8700027c53a949fc69533ca139c1
jmshinhwa <jmshinhwa@naver.com>
2026-01-30 02:15:38 +0900

    feat: add root layout

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'YooJin Hub',
+  description: 'Global Hub Funnel - Digital Products',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
